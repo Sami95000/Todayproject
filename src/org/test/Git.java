147e5b18f9c43d273fb60c805f766d5791b985f6
sami9500 <sami29091994@gmail.com>
2021-09-14 08:24:35 +0530

    FolderCreated

---
+++ src/org/test/Git.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Git {
+	private void name() {
+		System.out.println("name");
+	}
+
+	private void age() {
+		System.out.println("age");
+	}
+public static void main(String[] args) {
+	Git g = new Git();
+	g.name();
+	g.age();
+}
+}
